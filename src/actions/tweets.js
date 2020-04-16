43a9bbc246bbd14cd3c8f25267f650208657575b
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-04-16 16:32:18 +0500

    feat:add like button functionality

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,3 +1,6 @@
+import { saveLikeToggle } from "../utils/api";
+
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 
 export function receiveTweets(tweet) {
@@ -5,4 +8,26 @@ export function receiveTweets(tweet) {
     type: RECEIVE_TWEETS,
     tweet,
   };
+}
+
+function toggleTweet ({authedUser,hasLiked,id}){
+  return {
+    type:TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked,
+  }
+}
+
+export function handleToggleTweet(info){
+ return (dispatch)=>{
+   dispatch(toggleTweet(info))
+
+   return saveLikeToggle(info)
+    .catch((e)=>{
+      console.warn("Error in handleToggle: ", e)
+      dispatch(toggleTweet(info))
+      alert('There was an error liking this tweet. try again')
+    })
+ } 
 }
