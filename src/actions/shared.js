a00b5e99b8292591706c6eeb99ffb61d06d1bf93
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-04-16 15:11:09 +0500

    feat:build UI for Tweet component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import {receiveTweets} from '../actions/tweets'
 import {receiveUsers} from '../actions/users'
 import {setAuthedUser} from '../actions/authedUser'
+import {showLoading, hideLoading} from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData(){
     return (dispatch)=>{
-        getInitialData()
+        dispatch(showLoading())
+        return getInitialData()
         .then(({users,tweets})=>{
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,78 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux'
-import {formatTweet} from '../utils/helpers'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatTweet, formatDate } from "../utils/helpers";
+import { TiArrowBackOutline } from "react-icons/ti";
+import { TiHeartOutline } from "react-icons/ti";
+import { TiHeartFullOutline } from "react-icons/ti";
 
-class Tweet extends Component{
-    render(){
-      const { tweet} = this.props
-      if(tweet===null){
-        return (
-          <p>This tweet tweet doesnot existed</p>
-        )
-      }
-        console.log(this.props)
-        return(
-
-            <div className="tweet">
-
-            </div>
-        )
-    }
-}
-function mapStateToProps ({authedUser, users, tweets}, { id }) {
-    const tweet = tweets[id];
-    const parentTweet = tweet ? tweets[tweet.replyingTo]:null
-  
-    return {
-      authedUser,
-      tweet: tweet ?
-       formatTweet(tweet, users[tweet.author], authedUser,parentTweet)
-       : null
-    };
+class Tweet extends Component {
+  handleLike = (e) =>{
+    e.preventDefault()
   }
+  toParent = (e, id) => {
+    e.preventDefault();
+    //todo:redirect to some certain page
+  };
+  render() {
+    const { tweet } = this.props;
 
-export default connect(mapStateToProps)(Tweet)
+    if (tweet === null) {
+      return <p>This tweet tweet doesnot existed</p>;
+    }
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      hasLiked,
+      likes,
+      replies,
+      parent,
+    } = tweet;
+
+    return (
+      <div className="tweet">
+        <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+        <div className="tweet-info">
+        <div>
+          <span>{name}</span>
+          <div>{formatDate(timestamp)}</div>
+          {parent && (
+            <button
+              className="replying-to"
+              onClick={(e) => this.toParent(e, parent.id)}
+            >
+              Replying to @{parent.author}
+            </button>
+          )}
+          <p>{text}</p>
+          </div>
+        
+        <div className="tweet-icons">
+          <TiArrowBackOutline className="tweet-icon" />
+          <span>{replies !== 0 && replies}</span>
+          <button className="heart-button" onClick={this.handleLike}>
+              {hasLiked === true 
+              ? <TiHeartFullOutline color='#e0245e' className="tweet-icon" />
+              : <TiHeartOutline className="tweet-icon" />}
+          </button>
+          <span>{likes !== 0 && likes}</span>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+function mapStateToProps({ authedUser, users, tweets }, { id }) {
+  const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
+
+  return {
+    authedUser,
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
+  };
+}
+
+export default connect(mapStateToProps)(Tweet);
